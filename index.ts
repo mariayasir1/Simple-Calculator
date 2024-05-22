6750231b034307d1eb6158eac4d1ee037642afbd
mariayasir1 <mariahabib0300@gmail.com>
2024-05-22 15:50:57 -0700

    Adding a simple CLI calculator

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import { first } from "rxjs";
+
+console.log("\n\tWellcome to Maria's simple calculator\n")
+
+let answers = await inquirer.prompt([
+    {message:"Enter 1st number" , type:"number", name:"firstnumber"},
+    {message:"Enter 2nd number" , type:"number", name:"secondnumber"},
+    {
+        message:"Select one operator to perform operation",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subraction","Multiplication","Division"],
+    }
+]);
+if(answers.operator === "Addition"){
+    console.log(answers.firstnumber + answers.secondnumber);
+}
+else if(answers.operator === "Subraction"){
+    console.log(answers.firstnumber - answers.secondnumber);
+}
+else if(answers.operator === "Subraction"){
+    console.log(answers.firstnumber - answers.secondnumber);
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstnumber * answers.secondnumber);
+}
+else if(answers.operator === "Division"){
+    console.log(answers.firstnumber / answers.secondnumber);
+}
+else{
+    console.log("INVALID INPUT")
+}
